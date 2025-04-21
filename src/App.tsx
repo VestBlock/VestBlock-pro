a8e53cef6f9e0d06845b048e78e0348dd1b7e1bc
Shoeglitch <shoeglitch@gmail.com>
2025-04-21 15:20:50 -0500

    Route / to new Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
 import ProAnalysis from './pages/ProAnalysis';
 import DirectAnalysis from './pages/DirectAnalysis';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/pro-analysis" element={<ProAnalysis />} />
-      <Route path="/direct-analysis" element={<DirectAnalysis />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pro-analysis" element={<ProAnalysis />} />
+        <Route path="/direct-analysis" element={<DirectAnalysis />} />
+      </Routes>
+    </Router>
   );
 }
 
